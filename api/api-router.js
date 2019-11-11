0bc9121ee172f7b1e2860a1b4022c3bfcb2ad078
Timothy Sharkey <Techne3@gmail.com>
2019-11-11 14:49:09 -0700

    Added endpoints to post a user and password added auth middleware to validate

--- api/api-router.js
+++ api/api-router.js
@@ -1,11 +1,22 @@
 const router = require('express').Router()
 
+const authRouter= require('../auth/auth-router')
+const usersRouter=require('../users/users-router.js')
+
 const bcrypt = require('bcryptjs')
 
+router.use('/auth', authRouter)
+router.use('/users', usersRouter)
 
 router.get('/', (req,res)=> {
-    res.json({ api: "It's alive" });
+    res.json({ api: "It's  running, better catch it!" });
 })
 
-
+// router.post('/hash', (req,res)=> {
+//     //read password from the body
+//     let password = req.body.password
+//     const hash =bcrypt.hashSync(password, 14)
+//     //hash the password
+//     res.status(200).json({password, hash})
+// })
 module.exports=router
